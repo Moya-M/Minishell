1bcbb7a87e857ba95e9e8cf9ab4745ae25f57ac4
Manuel Moya <torox850@gmail.com>
2018-09-20 23:54:39 +0200

    normed expansions

--- srcs/expansions.c
+++ srcs/expansions.c
@@ -6,7 +6,7 @@
 /*   By: mmoya <mmoya@student.le-101.fr>            +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/09/19 16:29:14 by mmoya        #+#   ##    ##    #+#       */
-/*   Updated: 2018/09/20 23:45:52 by mmoya       ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/09/20 23:53:37 by mmoya       ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -55,33 +55,50 @@ int    	sh_expansions(const char *line, char **env)
 }
 */
 
-int    	sh_expansions(char **arg, char **env)
+static int	dollar_expansion(char **arg, char **env, int i)
+{
+	char *tmp;
+
+	tmp = arg[i];
+	if (sh_getenv(arg[i] + 1, env))
+	{
+		if (!(arg[i] = ft_strdup(sh_getenv(arg[i] + 1, env))))
+			return (1);
+	}
+	else
+	{
+		if (!(arg[i] = ft_strnew(0)))
+			return (1);
+	}
+	ft_strdel(&tmp);
+	return (0);
+}
+
+static int	tilde_expansion(char **arg, char **env, int i)
+{
+	char *tmp;
+
+	tmp = arg[i];
+	if (sh_getenv("HOME", env))
+	{
+		if (!(arg[i] = ft_strjoin(sh_getenv("HOME", env), arg[i] + 1)))
+			return (1);
+		ft_strdel(&tmp);
+	}
+	return (0);
+}
+
+int    		sh_expansions(char **arg, char **env)
 {
 	int		i;
-	char	*tmp;
 
 	i = 0;
-	(void)env;
 	while (arg[i])
 	{
 		if (arg[i][0] == '$')
-		{
-			tmp = arg[i];
-			if (sh_getenv(arg[i] + 1, env))
-				arg[i] = ft_strdup(sh_getenv(arg[i] + 1, env));
-			else
-				arg[i] = ft_strnew(0);
-			ft_strdel(&tmp);
-		}
+			dollar_expansion(arg, env, i);
 		if (arg[i][0] == '~')
-		{
-			tmp = arg[i];
-			if (sh_getenv("HOME", env))
-			{
-				arg[i] = ft_strjoin(sh_getenv("HOME", env), arg[i] + 1);
-				ft_strdel(&tmp);
-			}
-		}
+			tilde_expansion(arg, env, i);
 		i++;
 	}
 	return (0);

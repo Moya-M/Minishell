01047240a2f4e8da646735eaab992fc140d75a2b
Manuel Moya <torox850@gmail.com>
2018-09-20 23:56:58 +0200

    more norming

--- srcs/expansions.c
+++ srcs/expansions.c
@@ -6,54 +6,12 @@
 /*   By: mmoya <mmoya@student.le-101.fr>            +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/09/19 16:29:14 by mmoya        #+#   ##    ##    #+#       */
-/*   Updated: 2018/09/20 23:53:37 by mmoya       ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/09/20 23:55:29 by mmoya       ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-/*
-static int	env_exp(const char *line, int i, char **env)
-{
-	int		j;
-	int		k;
-	char	*tmp;
-
-	k = 0;
-	j = i;
-	while (!ft_isspace(line[j]) && line[j])
-		j++;
-	tmp = ft_strndup(line + i + 1, j - i - 1);
-	dprintf(1, "TMP = %s\n", tmp);
-	k += ft_strlen(sh_getenv(tmp, env));
-	ft_strdel(&tmp);
-	k -= j;
-	return (k);
-}
-
-static int	prompt_length(const char *line, char **env)
-{
-	int		i;
-	int		k;
-
-	i = 0;
-	k = 0;
-	while (line[i])
-	{
-		dprintf(1, "k = %i c = %c\n", k, line[i]);
-		if (line[i] == '$')
-			k += env_exp(line, i, env);
-		i++;
-		k++;
-	}
-	return (k);
-}
-
-int    	sh_expansions(const char *line, char **env)
-{
-	return (prompt_length(line, env));
-}
-*/
 
 static int	dollar_expansion(char **arg, char **env, int i)
 {
@@ -88,7 +46,7 @@ static int	tilde_expansion(char **arg, char **env, int i)
 	return (0);
 }
 
-int    		sh_expansions(char **arg, char **env)
+int			sh_expansions(char **arg, char **env)
 {
 	int		i;
 
